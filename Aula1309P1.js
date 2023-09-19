d1b6e5b1e347948a7e7acbe16078e1f3ac331744
PedroMachadoGRP <143435304+PedroMachadoGRP@users.noreply.github.com>
2023-09-19 13:55:07 -0300

    Realização de atividades dadas em aula

---
+++ Aula1309P1.js
@@ -0,0 +1,12 @@
+const nome = prompt ("Qual o seu nome?")
+const Core = prompt ("Qual a sua Cor favorita?")
+
+console.log("Meu nome é", nome, "e minha cor favorita é ",Core);
+alert(`Meu nome é ${nome} e minha cor favorita é ${Core}`)
+
+
+const Frase = prompt("Escreva uma frase que você goste")
+
+console.log(Frase.toUpperCase(Frase));
+console.log(Frase.replace("o","i"));
+console.log(Frase.length);
